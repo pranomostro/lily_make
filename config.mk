f070aeb26e3aaff9a069a1babb78c7d9f5d74c1d
pranomostro <pranomestro@gmail.com>
2016-09-08 23:45:49 +0200

    Updated makefile for version 0.17.

--- config.mk
+++ config.mk
@@ -9,12 +9,12 @@ AR=ar
 ARFLAGS=-rcs
 
 CC=cc
-CFLAGS=-DLILY_VERSION_DIR=\"0_16\" -Isrc
+CFLAGS=-DLILY_VERSION_DIR=\"0_17\" -Isrc
 LDFLAGS=-ldl -Isrc
 
 TARGET=lily
 LIB=src/liblily.a
 
-SRC=src/csiphash.c src/lily_cls_double.c src/lily_msgbuf.c src/lily_alloc.c src/lily_cls_integer.c src/lily_emitter.c src/lily_raiser.c src/lily_value.c src/lily_cls_dynamic.c src/lily_ast.c src/lily_cls_list.c src/lily_opcode_table.c src/lily_vm.c src/lily_cls_either.c src/lily_symtab.c src/lily_buffers.c src/lily_cls_option.c src/lily_lexer.c src/lily_parser.c src/lily_cls_file.c src/lily_type_maker.c src/lily_cls_boolean.c src/lily_cls_string.c src/lily_library.c src/lily_cls_function.c src/lily_pkg_builtin.c src/lily_type_system.c src/lily_cls_bytestring.c src/lily_membuf.c src/lily_cls_hash.c src/lily_pkg_sys.c src/lily_utf8.c em/sandbox.c
+SRC=src/csiphash.c src/lily_buffer_u16.c src/lily_generic_pool.c src/lily_msgbuf.c src/lily_raiser.c src/lily_type_system.c src/lily_alloc.c src/lily_code_iter.c src/lily_lexer.c src/lily_parser.c src/lily_string_pile.c src/lily_utf8.c src/lily_api_options.c src/lily_emitter.c src/lily_library.c src/lily_pkg_builtin.c src/lily_symtab.c src/lily_value_stack.c src/lily_api_value.c src/lily_expr.c src/lily_move.c src/lily_pkg_sys.c src/lily_type_maker.c src/lily_vm.c
 
 OBJ=$(SRC:.c=.o)
