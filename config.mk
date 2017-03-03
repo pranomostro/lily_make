b71dcc2de5aad68c951fd3b319fe3286a6241540
pranomostro <pranomestro@gmail.com>
2017-03-03 01:33:02 +0100

    Updated config.mk for lily version 1.0.

--- config.mk
+++ config.mk
@@ -9,12 +9,12 @@ AR=ar
 ARFLAGS=-rcs
 
 CC=cc
-CFLAGS=-DLILY_VERSION_DIR=\"0.18\" -Isrc
+CFLAGS=-DLILY_VERSION_DIR=\"1.0\" -Isrc
 LDFLAGS=-ldl -Isrc
 
 TARGET=lily
 LIB=src/liblily.a
 
-SRC=src/lily_alloc.c src/lily_api_options.c src/lily_api_value.c src/lily_buffer_u16.c src/lily_code_iter.c src/lily_emitter.c src/lily_expr.c src/lily_generic_pool.c src/lily_lexer.c src/lily_library.c src/lily_move.c src/lily_msgbuf.c src/lily_parser.c src/lily_pkg_builtin.c src/lily_pkg_random.c src/lily_pkg_sys.c src/lily_raiser.c src/lily_string_pile.c src/lily_symtab.c src/lily_type_maker.c src/lily_type_system.c src/lily_utf8.c src/lily_value_stack.c src/lily_vm.c src/st.c
+SRC=src/lily_alloc.c src/lily_api_value.c src/lily_buffer_u16.c src/lily_code_iter.c src/lily_emitter.c src/lily_expr.c src/lily_generic_pool.c src/lily_lexer.c src/lily_library.c src/lily_move.c src/lily_msgbuf.c src/lily_options.c src/lily_parser.c src/lily_pkg_builtin.c src/lily_pkg_random.c src/lily_pkg_sys.c src/lily_pkg_time.c src/lily_raiser.c src/lily_string_pile.c src/lily_symtab.c src/lily_type_maker.c src/lily_type_system.c src/lily_utf8.c src/lily_value_stack.c src/lily_vm.c src/st.c
 
 OBJ=$(SRC:.c=.o)
